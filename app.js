26f325de5808157132f5b0cc191f1e745d165861
Aniket Datta <datta.aniket7@gmail.com>
2023-07-22 19:35:26 +0530

    promise-example

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-function callbackHell() {
-  setTimeout(() => {
-    const data = { user: "oldMonk" };
-    console.log(data);
-    setTimeout(() => {
-      console.log("Data has been updated");
-    }, 1000);
-  }, 500);
-}
+const promise = new Promise((resolve, reject) => {
+  resolve({ user: "oldMonk" });
+});
 
-callbackHell();
+promise
+  .then((data) => {
+    const user = data;
+    return fetch("https://jsonplaceholder.typicode.com/todos/1");
+  })
+  .then((todos) => {
+    console.log(todos);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
