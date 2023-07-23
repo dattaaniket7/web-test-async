cd75d1f450a7775dbaac607b39e47cecdb31ae76
Aniket Datta <datta.aniket7@gmail.com>
2023-07-23 20:31:04 +0530

    async-await

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-fetch("https://jsonplaceholder.typico")
-  .then((data) => {
-    return data.json();
-  })
-  .then((jsonedData) => {
-    console.log(jsonedData);
-  })
-  .catch((err) => {
-    console.log("Oh nooo something went wrong");
-  })
-  .finally(() => {
-    console.log("well everything is done, we tried");
-  });
+async function getTodos() {
+  try {
+    const response = await fetch("https://jsonplacder.typicode.com/todos/1");
+    const data = await response.json();
+    console.log(data);
+    console.log(2);
+  } catch (err) {
+    console.log("Something went wrong...");
+    console.log(err);
+  }
+}
+
+getTodos();
